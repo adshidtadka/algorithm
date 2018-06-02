55ad44558e33645188bc259040a29a87632f61d8
Takaaki Sawa <tsawa@icn.cce.i.kyoto-u.ac.jp>
2018-06-02 15:01:29 +0900

    python

--- atcoder/AtCoder Beginners Selection/ABC081B - Shift only.py
+++ atcoder/AtCoder Beginners Selection/ABC081B - Shift only.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
 
+n = input()
+a = list(map(int, input().split()))
 
-s = list(input())
+isEven = True
 ans = 0
-for i in range(len(s)):
-	if s[i] == '1':
-		ans += 1
+while isEven == True:
+	for x in range(len(a)):
+		if a[x] % 2 == 1 :
+			isEven = False
+	if isEven == True:
+		for x in range(len(a)):
+			a[x] /= 2
+	else:
+		break
+	ans += 1
 
 print(ans)
