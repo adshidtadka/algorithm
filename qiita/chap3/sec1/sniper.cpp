2e763d987828e888720b70008ef8a323b9c8c6f7
adshidtadka <nextage900fx@gmail.com>
2020-05-27 16:04:37 +0900

    qiita: sec1を修正　

--- qiita/chap3/sec1/sniper.cpp
+++ qiita/chap3/sec1/sniper.cpp
@@ -1,111 +1,200 @@
-// c
-#ifndef _glibcxx_no_assert
-#include <cassert>
-#endif
+// include
+//------------------------------------------
+#include <algorithm>
+#include <bitset>
 #include <cctype>
-#include <cerrno>
-#include <cfloat>
-#include <ciso646>
-#include <climits>
-#include <clocale>
 #include <cmath>
-#include <csetjmp>
-#include <csignal>
-#include <cstdarg>
-#include <cstddef>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <ctime>
-
-#ifdef gxx_experimental_cxx0x
-#include <ccomplex>
-#include <cfenv>
-#include <cinttypes>
-#include <cstdbool>
-#include <cstdint>
-#include <ctgmath>
-#include <cwchar>
-#include <cwctype>
-#endif
-
-// c++
-#include <algorithm>
-#include <bitset>
-#include <complex>
 #include <deque>
-#include <exception>
-#include <fstream>
 #include <functional>
 #include <iomanip>
-#include <ios>
-#include <iosfwd>
 #include <iostream>
-#include <istream>
-#include <iterator>
-#include <limits>
 #include <list>
-#include <locale>
 #include <map>
-#include <memory>
-#include <new>
 #include <numeric>
-#include <ostream>
-#include <queue>
 #include <set>
 #include <sstream>
 #include <stack>
-#include <stdexcept>
-#include <streambuf>
 #include <string>
-#include <typeinfo>
 #include <utility>
-#include <valarray>
 #include <vector>
 
-#ifdef gxx_experimental_cxx0x
-#include <array>
-#include <atomic>
-#include <chrono>
-#include <condition_variable>
-#include <forward_list>
-#include <future>
-#include <initializer_list>
-#include <mutex>
-#include <random>
-#include <ratio>
-#include <regex>
-#include <system_error>
-#include <thread>
-#include <tuple>
-#include <type_traits>
-#include <typeindex>
-#include <unordered_map>
-#include <unordered_set>
-#endif
-#define rep(i, n) for (int i = 0; i < (n); ++i)
 using namespace std;
-using ll = long long;
-using p = pair<int, int>;
 
-int n;
-ll h[100001], s[100001];
+// conversion
+//------------------------------------------
+inline int toInt(string s) {
+    int v;
+    istringstream sin(s);
+    sin >> v;
+    return v;
+}
+template <class T>
+inline string toString(T x) {
+    ostringstream sout;
+    sout << x;
+    return sout.str();
+}
 
-bool isok(ll x) {
-    ll timetable[n];
-    memset(timetable, 0, sizeof(timetable));
-    for (int i = 0; i < n; i++) {
-        ll between = x - h[i];
-        if (between < 0) return false;
-        int cnt = between / s[i];
-        if (cnt >= n) cnt = n - 1;
-        while (timetable[cnt] == 1) {
-            cnt--;
-            if (cnt < 0) {
-                return false;
-            }
+// math
+//-------------------------------------------
+template <class T>
+inline T sqr(T x) {
+    return x * x;
+}
+
+// typedef
+//------------------------------------------
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int, int> PII;
+typedef long long LL;
+
+// container util
+//------------------------------------------
+#define ALL(a) (a).begin(), (a).end()
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define EACH(i, c) \
+    for (typeof((c).begin()) i = (c).begin(); i != (c).end(); ++i)
+#define EXIST(s, e) ((s).find(e) != (s).end())
+#define SORT(c) sort((c).begin(), (c).end())
+
+// repetition
+//------------------------------------------
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR(i, 0, n)
+#define REPI(itr, ds) for (auto itr = ds.begin(); itr != ds.end(); itr++)
+
+// constant
+//--------------------------------------------
+const double EPS = 1e-10;
+const double PI = acos(-1.0);
+
+// clear memory
+//--------------------------------------------
+#define CLR(a) memset((a), 0, sizeof(a))
+
+// vector
+//--------------------------------------------
+template <typename T>
+istream &operator>>(istream &is, vector<T> &vec) {
+    for (T &x : vec) is >> x;
+    return is;
+}
+
+// pair
+//--------------------------------------------
+template <typename T, typename U>
+ostream &operator<<(ostream &os, pair<T, U> &pair_var) {
+    os << "(" << pair_var.first << ", " << pair_var.second << ")";
+    return os;
+}
+
+// vector
+//--------------------------------------------
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &vec) {
+    os << "{";
+    for (int i = 0; i < vec.size(); i++) {
+        os << vec[i] << (i + 1 == vec.size() ? "" : ", ");
+    }
+    os << "}";
+    return os;
+}
+
+// list
+//--------------------------------------------
+template <typename T>
+ostream &operator<<(ostream &os, const list<T> &lst) {
+    os << "{";
+    REPI(itr, lst) {
+        os << *itr;
+        itr++;
+        if (itr != lst.end()) os << ", ";
+        itr--;
+    }
+    os << "}";
+    return os;
+}
+
+// map
+//--------------------------------------------
+template <typename T, typename U>
+ostream &operator<<(ostream &os, map<T, U> &map_var) {
+    os << "{";
+    REPI(itr, map_var) {
+        os << *itr;
+        itr++;
+        if (itr != map_var.end()) os << ", ";
+        itr--;
+    }
+    os << "}";
+    return os;
+}
+
+// set
+//--------------------------------------------
+template <typename T>
+ostream &operator<<(ostream &os, set<T> &set_var) {
+    os << "{";
+    REPI(itr, set_var) {
+        os << *itr;
+        itr++;
+        if (itr != set_var.end()) os << ", ";
+        itr--;
+    }
+    os << "}";
+    return os;
+}
+
+// dump
+//--------------------------------------------
+#define DUMPOUT cerr
+
+void dump_func() { DUMPOUT << endl; }
+template <class Head, class... Tail>
+void dump_func(Head &&head, Tail &&... tail) {
+    DUMPOUT << head;
+    if (sizeof...(Tail) > 0) {
+        DUMPOUT << ", ";
+    }
+    dump_func(std::move(tail)...);
+}
+#ifdef DEBUG_
+#define DEB
+#define dump(...)                                                       \
+    DUMPOUT << "  " << string(#__VA_ARGS__) << ": "                     \
+            << "[" << to_string(__LINE__) << ":" << __FUNCTION__ << "]" \
+            << endl                                                     \
+            << "    ",                                                  \
+        dump_func(__VA_ARGS__)
+#else
+#define DEB if (false)
+#define dump(...)
+#endif
+
+LL n;
+LL h[100001], s[100001];
+
+bool isok(LL x) {
+    VI count(n);
+    REP(i, n) {
+        if (h[i] > x) {
+            return false;
+        } else {
+            count[min(n - 1, (x - h[i]) / s[i])]++;
         }
-        timetable[cnt] = 1;
+    }
+    REP(j, n - 1) { count[j + 1] += count[j]; }
+    REP(j, n) {
+        if (count[j] > j + 1) return false;
     }
     return true;
 }
@@ -115,29 +204,22 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> h[i] >> s[i];
     }
-    ll inf = 1000;
-    ll right = inf;
-    ll left = 0;
-    ll middle = inf / 2;
-    while (true) {
+    LL inf = 1e18;
+    LL right = inf;
+    LL left = 0;
+    LL middle;
+    while (right - left > 1) {
+        middle = (right + left) / 2;
         if (isok(middle)) {
             right = middle;
         } else {
             left = middle;
         }
-        middle = (right - left) / 2 + left;
-        if (right == left) {
-            cout << right << endl;
-            return 0;
-        } else if ((right - left) == 1) {
-            if (isok(right)) {
-                cout << right << endl;
-                return 0;
-            } else {
-                cout << left << endl;
-                return 0;
-            }
-        }
+    }
+    if (isok(right)) {
+        cout << right << endl;
+    } else {
+        cout << left << endl;
     }
     return 0;
 }
