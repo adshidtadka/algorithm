fdd77e33f3483be36a8ff44850fe15f44b478efe
adshidtadka <nextage900fx@gmail.com>
2020-06-04 06:49:57 +0900

    column and supplement

---
+++ qiita/chap3/sec3/supplement.cpp
@@ -0,0 +1,241 @@
+// include
+//------------------------------------------
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+// conversion
+//------------------------------------------
+inline int toInt(string s) {
+    int v;
+    istringstream sin(s);
+    sin >> v;
+    return v;
+}
+template <class T>
+inline string toString(T x) {
+    ostringstream sout;
+    sout << x;
+    return sout.str();
+}
+
+// math
+//-------------------------------------------
+template <class T>
+inline T sqr(T x) {
+    return x * x;
+}
+
+// typedef
+//------------------------------------------
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int, int> PII;
+typedef long long LL;
+
+// container util
+//------------------------------------------
+#define ALL(a) (a).begin(), (a).end()
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define EACH(i, c) \
+    for (typeof((c).begin()) i = (c).begin(); i != (c).end(); ++i)
+#define EXIST(s, e) ((s).find(e) != (s).end())
+#define SORT(c) sort((c).begin(), (c).end())
+
+// repetition
+//------------------------------------------
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR(i, 0, n)
+#define REPI(itr, ds) for (auto itr = ds.begin(); itr != ds.end(); itr++)
+
+// constant
+//--------------------------------------------
+const double EPS = 1e-10;
+const double PI = acos(-1.0);
+
+// clear memory
+//--------------------------------------------
+#define CLR(a) memset((a), 0, sizeof(a))
+
+// vector
+//--------------------------------------------
+template <typename T>
+istream &operator>>(istream &is, vector<T> &vec) {
+    for (T &x : vec) is >> x;
+    return is;
+}
+
+// pair
+//--------------------------------------------
+template <typename T, typename U>
+ostream &operator<<(ostream &os, pair<T, U> &pair_var) {
+    os << "(" << pair_var.first << ", " << pair_var.second << ")";
+    return os;
+}
+
+// vector
+//--------------------------------------------
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &vec) {
+    os << "{";
+    for (int i = 0; i < vec.size(); i++) {
+        os << vec[i] << (i + 1 == vec.size() ? "" : ", ");
+    }
+    os << "}";
+    return os;
+}
+
+// list
+//--------------------------------------------
+template <typename T>
+ostream &operator<<(ostream &os, const list<T> &lst) {
+    os << "{";
+    REPI(itr, lst) {
+        os << *itr;
+        itr++;
+        if (itr != lst.end()) os << ", ";
+        itr--;
+    }
+    os << "}";
+    return os;
+}
+
+// map
+//--------------------------------------------
+template <typename T, typename U>
+ostream &operator<<(ostream &os, map<T, U> &map_var) {
+    os << "{";
+    REPI(itr, map_var) {
+        os << *itr;
+        itr++;
+        if (itr != map_var.end()) os << ", ";
+        itr--;
+    }
+    os << "}";
+    return os;
+}
+
+// set
+//--------------------------------------------
+template <typename T>
+ostream &operator<<(ostream &os, set<T> &set_var) {
+    os << "{";
+    REPI(itr, set_var) {
+        os << *itr;
+        itr++;
+        if (itr != set_var.end()) os << ", ";
+        itr--;
+    }
+    os << "}";
+    return os;
+}
+
+// dump
+//--------------------------------------------
+#define DUMPOUT cerr
+
+void dump_func() { DUMPOUT << endl; }
+template <class Head, class... Tail>
+void dump_func(Head &&head, Tail &&... tail) {
+    DUMPOUT << head;
+    if (sizeof...(Tail) > 0) {
+        DUMPOUT << ", ";
+    }
+    dump_func(std::move(tail)...);
+}
+#ifdef DEBUG_
+#define DEB
+#define dump(...)                                                       \
+    DUMPOUT << "  " << string(#__VA_ARGS__) << ": "                     \
+            << "[" << to_string(__LINE__) << ":" << __FUNCTION__ << "]" \
+            << endl                                                     \
+            << "    ",                                                  \
+        dump_func(__VA_ARGS__)
+#else
+#define DEB if (false)
+#define dump(...)
+#endif
+
+// auto mod int
+// https://youtu.be/L8grWxBlIZ4?t=9858
+// https://youtu.be/ERZuLAxZffQ?t=4807 : optimize
+// https://youtu.be/8uowVvQ_-Mo?t=1329 : division
+const int mod = 1000000007;
+struct mint {
+    LL x;  // typedef long long LL;
+    mint(LL x = 0) : x((x % mod + mod) % mod) {}
+    mint operator-() const { return mint(-x); }
+    mint &operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint &operator-=(const mint a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint &operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+    mint operator+(const mint a) const { return mint(*this) += a; }
+    mint operator-(const mint a) const { return mint(*this) -= a; }
+    mint operator*(const mint a) const { return mint(*this) *= a; }
+    mint pow(LL t) const {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const { return pow(mod - 2); }
+    mint &operator/=(const mint a) { return *this *= a.inv(); }
+    mint operator/(const mint a) const { return mint(*this) /= a; }
+};
+
+istream &operator>>(istream &is, const mint &a) { return is >> a.x; }
+ostream &operator<<(ostream &os, const mint &a) { return os << a.x; }
+
+int n, m;
+int f[100001];
+
+int main(int argc, char *argv[]) {
+    cin >> n >> m;
+    REP(i, n) cin >> f[i];
+
+    mint ans = 0;
+    int dp[n];
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        for(int j = i; j > 0; j--) {
+            
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
