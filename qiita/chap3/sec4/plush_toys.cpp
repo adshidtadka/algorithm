712ffe771e23777242b48cf4edea90fbd5ec7ab3
adshidtadka <nextage900fx@gmail.com>
2020-07-09 23:42:18 +0900

    第8回

---
+++ qiita/chap3/sec4/plush_toys.cpp
@@ -0,0 +1,205 @@
+// include
+//------------------------------------------
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// conversion
+//------------------------------------------
+inline int toInt(string s) {
+    int v;
+    istringstream sin(s);
+    sin >> v;
+    return v;
+}
+template <class T>
+inline string toString(T x) {
+    ostringstream sout;
+    sout << x;
+    return sout.str();
+}
+
+// math
+//-------------------------------------------
+template <class T>
+inline T sqr(T x) {
+    return x * x;
+}
+
+// typedef
+//------------------------------------------
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int, int> PII;
+typedef long long LL;
+
+// container util
+//------------------------------------------
+#define ALL(a) (a).begin(), (a).end()
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define EACH(i, c) \
+    for (typeof((c).begin()) i = (c).begin(); i != (c).end(); ++i)
+#define EXIST(s, e) ((s).find(e) != (s).end())
+#define SORT(c) sort((c).begin(), (c).end())
+
+// repetition
+//------------------------------------------
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR(i, 0, n)
+#define REPI(itr, ds) for (auto itr = ds.begin(); itr != ds.end(); itr++)
+
+// constant
+//--------------------------------------------
+const double EPS = 1e-10;
+const double PI = acos(-1.0);
+const int INF_INT = 2147483647;
+const LL INF_LL = 9223372036854775807;
+
+// clear memory
+//--------------------------------------------
+#define CLR(a) memset((a), 0, sizeof(a))
+
+// vector
+//--------------------------------------------
+template <typename T>
+istream &operator>>(istream &is, vector<T> &vec) {
+    for (T &x : vec) is >> x;
+    return is;
+}
+
+// pair
+//--------------------------------------------
+template <typename T, typename U>
+ostream &operator<<(ostream &os, pair<T, U> &pair_var) {
+    os << "(" << pair_var.first << ", " << pair_var.second << ")";
+    return os;
+}
+
+// vector
+//--------------------------------------------
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &vec) {
+    os << "{";
+    for (int i = 0; i < vec.size(); i++) {
+        os << vec[i] << (i + 1 == vec.size() ? "" : ", ");
+    }
+    os << "}";
+    return os;
+}
+
+// list
+//--------------------------------------------
+template <typename T>
+ostream &operator<<(ostream &os, const list<T> &lst) {
+    os << "{";
+    REPI(itr, lst) {
+        os << *itr;
+        itr++;
+        if (itr != lst.end()) os << ", ";
+        itr--;
+    }
+    os << "}";
+    return os;
+}
+
+// map
+//--------------------------------------------
+template <typename T, typename U>
+ostream &operator<<(ostream &os, map<T, U> &map_var) {
+    os << "{";
+    REPI(itr, map_var) {
+        os << *itr;
+        itr++;
+        if (itr != map_var.end()) os << ", ";
+        itr--;
+    }
+    os << "}";
+    return os;
+}
+
+// set
+//--------------------------------------------
+template <typename T>
+ostream &operator<<(ostream &os, set<T> &set_var) {
+    os << "{";
+    REPI(itr, set_var) {
+        os << *itr;
+        itr++;
+        if (itr != set_var.end()) os << ", ";
+        itr--;
+    }
+    os << "}";
+    return os;
+}
+
+// dump
+//--------------------------------------------
+#define DUMPOUT cerr
+
+void dump_func() { DUMPOUT << endl; }
+template <class Head, class... Tail>
+void dump_func(Head &&head, Tail &&... tail) {
+    DUMPOUT << head;
+    if (sizeof...(Tail) > 0) {
+        DUMPOUT << ", ";
+    }
+    dump_func(std::move(tail)...);
+}
+#ifdef DEBUG_
+#define DEB
+#define dump(...)                                                       \
+    DUMPOUT << "  " << string(#__VA_ARGS__) << ": "                     \
+            << "[" << to_string(__LINE__) << ":" << __FUNCTION__ << "]" \
+            << endl                                                     \
+            << "    ",                                                  \
+        dump_func(__VA_ARGS__)
+#else
+#define DEB if (false)
+#define dump(...)
+#endif
+
+int n, m;
+int a[100001];
+int b[21];
+
+int main(void) {
+    cin >> n >> m;
+    REP(i, n) cin >> a[i];
+    REP(j, m) b[j] = 0;
+    REP(i, n) b[a[i]]++;
+
+    int dp[1 << m];
+    REP(i, 1 << m) dp[i] = INF_INT;
+    dp[0] = 0;
+    for (int s = 1; s < 1 << m; s++) {
+        VI S;
+        int total = 0;
+        REP(i, m) {
+            if (s & (1 << i)) {
+                S.PB(i);
+                total += b[i + 1];
+            };
+        }
+
+        // 1つ取り除いた部分集合のdpの値にそのひとつの値に関する相違数の合計
+        int sMin = INF_INT;
+        REPI(it, S) {
+            // 1つ取り除いた部分集合
+            int sSub = s & ~(1 << *it);
+            int cnt = 0;
+            for (int i = total - b[*it + 1]; i < total; i++) {
+                if (i == *it) cnt++;
+            }
+            dump(dp[sSub]);
+            dump(cnt);
+            sMin = min(sMin, dp[sSub] + cnt);
+        }
+        dp[s] = sMin;
+    }
+    cout << dp[(1 << m) - 1] << endl;
+    return 0;
+}
