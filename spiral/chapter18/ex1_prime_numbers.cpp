97a3c1d8916d7413cb42636119a9d9bf11e3d03c
adshidtadka <nextage900fx@gmail.com>
2020-07-26 23:56:57 +0900

    整数論

---
+++ spiral/chapter18/ex1_prime_numbers.cpp
@@ -0,0 +1,232 @@
+// include
+//------------------------------------------
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// conversion
+//------------------------------------------
+inline int toInt(string s) {
+    int v;
+    istringstream sin(s);
+    sin >> v;
+    return v;
+}
+template <class T>
+inline string toString(T x) {
+    ostringstream sout;
+    sout << x;
+    return sout.str();
+}
+
+// math
+//-------------------------------------------
+template <class T>
+inline T sqr(T x) {
+    return x * x;
+}
+
+// typedef
+//------------------------------------------
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int, int> PII;
+typedef long long LL;
+
+// container util
+//------------------------------------------
+#define ALL(a) (a).begin(), (a).end()
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define EACH(i, c) \
+    for (typeof((c).begin()) i = (c).begin(); i != (c).end(); ++i)
+#define EXIST(s, e) ((s).find(e) != (s).end())
+#define SORT(c) sort((c).begin(), (c).end())
+
+// repetition
+//------------------------------------------
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR(i, 0, n)
+#define REPI(itr, ds) for (auto itr = ds.begin(); itr != ds.end(); itr++)
+
+// constant
+//--------------------------------------------
+const double EPS = 1e-10;
+const double PI = acos(-1.0);
+const int INF_INT = 2147483647;
+const LL INF_LL = 9223372036854775807;
+
+// clear memory
+//--------------------------------------------
+#define CLR(a) memset((a), 0, sizeof(a))
+
+// vector
+//--------------------------------------------
+template <typename T>
+istream &operator>>(istream &is, vector<T> &vec) {
+    for (T &x : vec) is >> x;
+    return is;
+}
+
+// pair
+//--------------------------------------------
+template <typename T, typename U>
+ostream &operator<<(ostream &os, pair<T, U> &pair_var) {
+    os << "(" << pair_var.first << ", " << pair_var.second << ")";
+    return os;
+}
+
+// vector
+//--------------------------------------------
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &vec) {
+    os << "{";
+    for (int i = 0; i < vec.size(); i++) {
+        os << vec[i] << (i + 1 == vec.size() ? "" : ", ");
+    }
+    os << "}";
+    return os;
+}
+
+// list
+//--------------------------------------------
+template <typename T>
+ostream &operator<<(ostream &os, const list<T> &lst) {
+    os << "{";
+    REPI(itr, lst) {
+        os << *itr;
+        itr++;
+        if (itr != lst.end()) os << ", ";
+        itr--;
+    }
+    os << "}";
+    return os;
+}
+
+// map
+//--------------------------------------------
+template <typename T, typename U>
+ostream &operator<<(ostream &os, map<T, U> &map_var) {
+    os << "{";
+    REPI(itr, map_var) {
+        os << *itr;
+        itr++;
+        if (itr != map_var.end()) os << ", ";
+        itr--;
+    }
+    os << "}";
+    return os;
+}
+
+// set
+//--------------------------------------------
+template <typename T>
+ostream &operator<<(ostream &os, set<T> &set_var) {
+    os << "{";
+    REPI(itr, set_var) {
+        os << *itr;
+        itr++;
+        if (itr != set_var.end()) os << ", ";
+        itr--;
+    }
+    os << "}";
+    return os;
+}
+
+// dump
+//--------------------------------------------
+#define DUMPOUT cerr
+
+void dump_func() { DUMPOUT << endl; }
+template <class Head, class... Tail>
+void dump_func(Head &&head, Tail &&... tail) {
+    DUMPOUT << head;
+    if (sizeof...(Tail) > 0) {
+        DUMPOUT << ", ";
+    }
+    dump_func(std::move(tail)...);
+}
+#ifdef DEBUG_
+#define DEB
+#define dump(...)                                                       \
+    DUMPOUT << "  " << string(#__VA_ARGS__) << ": "                     \
+            << "[" << to_string(__LINE__) << ":" << __FUNCTION__ << "]" \
+            << endl                                                     \
+            << "    ",                                                  \
+        dump_func(__VA_ARGS__)
+#else
+#define DEB if (false)
+#define dump(...)
+#endif
+
+// 素数判定
+bool isPrime(int n) {
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+// 約数の列挙
+vector<int> divisor(int n) {
+    vector<int> res;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            res.push_back(i);
+            if (i != n / i) {
+                res.push_back(n / i);
+            }
+        }
+    }
+    return res;
+}
+
+// 素因数分解
+map<int, int> primeFactor(int n) {
+    map<int, int> res;
+    for (int i = 2; i * i <= n; i++) {
+        while (n % i == 0) {
+            ++res[i];
+            n /= i;
+        }
+    }
+    if (n != 1) {
+        res[n] = 1;
+    }
+    return res;
+}
+
+const int MAX_N = 1000000;
+int prime[MAX_N];
+bool primeTable[MAX_N + 1];
+
+int sieve(int n) {
+    int p = 0;
+    for (int i = 0; i <= n; i++) {
+        primeTable[i] = true;
+    }
+    primeTable[0] = primeTable[1] = false;
+    for (int i = 2; i <= n; i++) {
+        if (primeTable[i]) {
+            prime[p++] = i;
+            for (int j = 2 * i; j <= n; j += i) {
+                primeTable[j] = false;
+            }
+        }
+    }
+    return p;
+}
+
+int main(void) {
+    int n;
+    cin >> n;
+    int x[n];
+    REP(i, n) cin >> x[i];
+    int cnt = 0;
+    REP(i, n) if (isPrime(x[i])) cnt++;
+    cout << cnt << endl;
+    return 0;
+}
