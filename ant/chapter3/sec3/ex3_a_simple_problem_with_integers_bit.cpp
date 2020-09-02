c612dfe7b559e45be5adc394fb6499b2c1cbfd97
adshidtadka <nextage900fx@gmail.com>
2020-09-02 09:51:09 +0900

    a simple problem with integers

---
+++ ant/chapter3/sec3/ex3_a_simple_problem_with_integers_bit.cpp
@@ -0,0 +1,228 @@
+// include
+//------------------------------------------
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// conversion
+//------------------------------------------
+inline int toInt(string s) {
+    int v;
+    istringstream sin(s);
+    sin >> v;
+    return v;
+}
+template <class T>
+inline string toString(T x) {
+    ostringstream sout;
+    sout << x;
+    return sout.str();
+}
+
+// math
+//-------------------------------------------
+template <class T>
+inline T sqr(T x) {
+    return x * x;
+}
+
+// typedef
+//------------------------------------------
+typedef long long LL;
+typedef unsigned long long ULL;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int, int> PII;
+typedef vector<LL> VL;
+typedef vector<VL> VVL;
+template <class T>
+using VEC = vector<T>;
+template <class T>
+using VVEC = vector<VEC<T>>;
+
+// container util
+//------------------------------------------
+#define ALL(a) (a).begin(), (a).end()
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define EACH(i, c) \
+    for (typeof((c).begin()) i = (c).begin(); i != (c).end(); ++i)
+#define EXIST(s, e) ((s).find(e) != (s).end())
+#define SORT(c) sort((c).begin(), (c).end())
+
+// repetition
+//------------------------------------------
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR(i, 0, n)
+#define REPI(itr, ds) for (auto itr = ds.begin(); itr != ds.end(); itr++)
+
+// constant
+//--------------------------------------------
+const double EPS = 1e-10;
+const double PI = acos(-1.0);
+const int INF_INT = 2147483647;
+const LL INF_LL = 9223372036854775807;
+const int MOD = 1000000007;
+
+// clear memory
+//--------------------------------------------
+#define CLR(a) memset((a), 0, sizeof(a))
+
+// vector
+//--------------------------------------------
+template <typename T>
+istream &operator>>(istream &is, vector<T> &vec) {
+    for (T &x : vec) is >> x;
+    return is;
+}
+
+// pair
+//--------------------------------------------
+template <typename T, typename U>
+ostream &operator<<(ostream &os, pair<T, U> &pair_var) {
+    os << "(" << pair_var.first << ", " << pair_var.second << ")";
+    return os;
+}
+
+// vector
+//--------------------------------------------
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &vec) {
+    os << "{";
+    for (int i = 0; i < vec.size(); i++) {
+        os << vec[i] << (i + 1 == vec.size() ? "" : ", ");
+    }
+    os << "}";
+    return os;
+}
+
+// list
+//--------------------------------------------
+template <typename T>
+ostream &operator<<(ostream &os, const list<T> &lst) {
+    os << "{";
+    REPI(itr, lst) {
+        os << *itr;
+        itr++;
+        if (itr != lst.end()) os << ", ";
+        itr--;
+    }
+    os << "}";
+    return os;
+}
+
+// map
+//--------------------------------------------
+template <typename T, typename U>
+ostream &operator<<(ostream &os, map<T, U> &map_var) {
+    os << "{";
+    REPI(itr, map_var) {
+        os << *itr;
+        itr++;
+        if (itr != map_var.end()) os << ", ";
+        itr--;
+    }
+    os << "}";
+    return os;
+}
+
+// set
+//--------------------------------------------
+template <typename T>
+ostream &operator<<(ostream &os, set<T> &set_var) {
+    os << "{";
+    REPI(itr, set_var) {
+        os << *itr;
+        itr++;
+        if (itr != set_var.end()) os << ", ";
+        itr--;
+    }
+    os << "}";
+    return os;
+}
+
+// dump
+//--------------------------------------------
+#define DUMPOUT cerr
+
+void dump_func() { DUMPOUT << endl; }
+template <class Head, class... Tail>
+void dump_func(Head &&head, Tail &&... tail) {
+    DUMPOUT << head;
+    if (sizeof...(Tail) > 0) {
+        DUMPOUT << ", ";
+    }
+    dump_func(std::move(tail)...);
+}
+#ifdef DEBUG_
+#define DEB
+#define dump(...)                                                       \
+    DUMPOUT << "  " << string(#__VA_ARGS__) << ": "                     \
+            << "[" << to_string(__LINE__) << ":" << __FUNCTION__ << "]" \
+            << endl                                                     \
+            << "    ",                                                  \
+        dump_func(__VA_ARGS__)
+#else
+#define DEB if (false)
+#define dump(...)
+#endif
+
+const int DAT_SIZE = (1 << 18) - 1;
+const int MAX_N = 100000;
+
+int N, Q;
+int A[MAX_N];
+char T[MAX_N];
+int L[MAX_N], R[MAX_N], X[MAX_N];
+
+LL data[DAT_SIZE], datb[DAT_SIZE];
+
+// add x to [a, b)
+// k is coresponded to [l, r)
+void add(int a, int b, int x, int k, int l, int r) {
+    if (a <= l && r <= b) {
+        data[k] += x;
+    } else if (l < b && a < r) {
+        add(a, b, x, k * 2 + 1, l, (l + r) / 2);
+        add(a, b, x, k + 2 + 2, (l + r) / 2, r);
+        datb[k] += (min(b, r) - max(a, l)) * x;
+    }
+}
+
+LL sum(int a, int b, int k, int l, int r) {
+    if (b <= l || r <= a)
+        return 0;
+    else if (a <= l && r <= b)
+        return data[k] * (r - l) + datb[k];
+    else {
+        LL res = (min(b, r) - max(a, l)) * data[k];
+        res += sum(a, b, k * 2 + 1, l, (l + r) / 2);
+        res += sum(a, b, k * 2 + 2, (l + r) / 2, r);
+        return res;
+    }
+}
+
+int main(void) {
+    cin >> N >> Q;
+    REP(i, N) cin >> A[i];
+
+    REP(i, Q) {
+        cin >> T[i];
+        cin >> L[i] >> R[i];
+        if (T[i] == 'C') cin >> X[i];
+    }
+
+    REP(i, N) add(i, i + 1, A[i], 0, 0, N);
+
+    REP(i, Q) {
+        if (T[i] == 'C')
+            add(L[i] - 1, R[i], X[i], 0, 0, N);
+        else
+            printf("%lld\n", sum(L[i] - 1, R[i], 0, 0, N));
+    }
+    return 0;
+}
